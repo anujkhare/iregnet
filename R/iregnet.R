a259586c4cb894acbb937b56fb0f4b32c803c7f0
Anuj Khare <khareanuj18@gmail.com>
2016-07-20 11:54:28 +0530

    temp changes in R

--- R/iregnet.R
+++ R/iregnet.R
@@ -6,39 +6,37 @@
 # TODO: optimization params need to be implemented
 # TODO: checks like - can't provide scale with exp
 iregnet <- function(x, y,
-                    family=c("gaussian", "logistic", "loggaussian", "extreme value", "exponential"), alpha=1,
+                    family=c("gaussian", "logistic", "loggaussian", "extreme_value", "exponential"), alpha=1,
                     lambda=double(0), num_lambda=100, flag_debug=0, intercept=T, standardize=F, scale_init=NA, estimate_scale=T,
 										maxiter=1000, threshold=1e-4, unreg_sol=T) {
 
   # Parameter validation ===============================================
   # alpha should be between 0 and 1
-  if (alpha > 1) {
-    warning ("alpha > 1; setting to 1");
-    alpha <- 1;
+  if (0 > alpha || alpha > 1) {
+    stop("alpha should be between 0 and 1")
   }
-  if (alpha < 0) {
-    warning ("alpha<0; set to 0");
-    alpha <- 0;
-  }
-  alpha <- as.double(alpha);
 
 	if (estimate_scale == F && is.na(scale_init))
 		stop("Value of scale required if scale is not estimated")
 
-  # family should be one of ""
+  # family should be one of those listed
   family <- match.arg(family)
 
   # x should be a matrix with atleast two columns (TODO: why glmnet requires 2 or more cols?)
     # TODO: NAs in x? should be numeric
-  dim_x <- dim(x);    # LIST
-  if (is.null(dim_x) | dim_x[2] <= 1)
-    stop("x should be a matrix with 2 or more columns");
+  if (!is.matrix(x)) stop("x should be a matrix with 2 or more columns")
+  if (ncol(x) <= 1) stop("x should be a matrix with 2 or more columns")
 
-  n_obs  <- dim_x[1];   # LIST
-  n_vars <- dim_x[2];   # LIST
+  n_obs  <- nrow(x)   # LIST
+  n_vars <- ncol(x)   # LIST
 
   # y should be a matrix with 2 columns correspoding to the left and right times
   # NA or Inf/-Inf can be used for censored entries
+  if (is.matrix(y)){
+
+  # OR y can be a Surv object from the survival library
+  } else if (survival::is.Surv(y)) {
+  }
   dim_y <- dim(y);      # LIST
   if (is.null(dim_y) | dim_y[2] != 2)
    stop("y should be a matrix with 2 columns");
