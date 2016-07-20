10e00299f253cea02d1286780977b0726805157e
Anuj Khare <khareanuj18@gmail.com>
2016-07-20 13:07:51 +0530

    iregnet.R: error handling, etc

--- R/iregnet.R
+++ R/iregnet.R
@@ -11,10 +11,7 @@ iregnet <- function(x, y,
 										maxiter=1000, threshold=1e-4, unreg_sol=T, eps_lambda=NA) {
 
   # Parameter validation ===============================================
-  # alpha should be between 0 and 1
-  if (0 > alpha || alpha > 1) {
-    stop("alpha should be between 0 and 1")
-  }
+  stopifnot_error("alpha should be between 0 and 1", 0 <= alpha, alpha <= 1)
 
 	if (estimate_scale == F && is.na(scale_init))
 		stop("Value of scale required if scale is not estimated")
@@ -22,28 +19,21 @@ iregnet <- function(x, y,
   # family should be one of those listed
   family <- match.arg(family)
 
-  # x should be a matrix with atleast two columns (TODO: why glmnet requires 2 or more cols?)
-    # TODO: NAs in x? should be numeric
-  if (!is.matrix(x)) stop("x should be a matrix with 2 or more columns")
-  if (ncol(x) <= 1) stop("x should be a matrix with 2 or more columns")
+  # TODO: NAs in x? should be numeric
+  stopifnot_error("x should be a matrix with 2 or more columns", is.matrix(x), ncol(x) > 1)
 
-  n_obs  <- nrow(x)   # LIST
-  n_vars <- ncol(x)   # LIST
+  n_obs  <- nrow(x)
+  n_vars <- ncol(x)
 
   # y should be a matrix with 2 columns correspoding to the left and right times
   # NA or Inf/-Inf can be used for censored entries
-  if (is.matrix(y)){
+  stopifnot_error("y should be a 2 column matrix, or a Surv object", is.matrix(y) || survival::is.Surv(y))
 
-  # OR y can be a Surv object from the survival library
+  if (is.matrix(y)){
+    stopifnot_error("y should be a 2 column matrix with nrow(y) = nrow(x)", ncol(y) == 2, nrow(y) == n_obs)
+
   } else if (survival::is.Surv(y)) {
   }
-  dim_y <- dim(y);      # LIST
-  if (is.null(dim_y) | dim_y[2] != 2)
-   stop("y should be a matrix with 2 columns");
-
-  # length of x and y should be the same
-  if (dim_y[1] != n_obs)
-    stop("number of rows in x and y don't match");
 
 	# Append col of 1's for the intercept
 	if (intercept)
@@ -51,7 +41,22 @@ iregnet <- function(x, y,
 
   if (is.na(eps_lambda))
     eps_lambda <- ifelse(n_obs < n_vars, 0.01, 0.0001)
+  stopifnot_error("eps_lambda should be in [0, 1)", 0 <= eps_lambda && eps_lambda < 1)
+
   # Call the actual fit method
-  fit_cpp(x, y, family, alpha, lambda_path=lambda, num_lambda=num_lambda, intercept=intercept, scale_init=scale_init, max_iter=maxiter,
-            flag_standardize_x = standardize, threshold=threshold, estimate_scale=estimate_scale, unreg_sol=unreg_sol, eps_lambda=eps_lambda);
+  fit <- fit_cpp(x, y, family, alpha, lambda_path=lambda, num_lambda=num_lambda, intercept=intercept, scale_init=scale_init, max_iter=maxiter,
+                 flag_standardize_x = standardize, threshold=threshold, estimate_scale=estimate_scale, unreg_sol=unreg_sol, eps_lambda=eps_lambda);
+
+  fit$call <- match.call()
+  fit
+}
+
+# like stopifnot, but with a custom error message
+stopifnot_error <- function(err_message, ...)
+{
+  n <- length(ll <- list(...))
+  for (i in 1:n)
+    if (!(is.logical(r <- ll[[i]]) && !anyNA(r) && all(r))) {
+      stop(err_message)
+    }
 }
