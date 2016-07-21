c96663fd3144c4c8cbf90a1d371dc23504663838
Anuj Khare <khareanuj18@gmail.com>
2016-07-21 18:43:42 +0530

    fix tab identation in files

--- src/iregnet_fit.cpp
+++ src/iregnet_fit.cpp
@@ -4,11 +4,13 @@
 
 void get_y_means (Rcpp::NumericMatrix &y, IREG_CENSORING *status, double *ym);
 static inline double soft_threshold (double x, double lambda);
+static double get_init_var (double *ym, IREG_CENSORING *status, ull n, IREG_DIST dist);
+static double get_init_intercept (double *mu, double *w, double *ym, ull n_obs);
 static void standardize_x (Rcpp::NumericMatrix X,
                            double *mean_x, double *std_x,
                            bool intercept);
-static double get_init_var (double *ym, IREG_CENSORING *status, ull n, IREG_DIST dist);
-static double get_init_intercept (double *mu, double *w, double *ym, ull n_obs);
+static inline double compute_lambda_max(Rcpp::NumericMatrix X, double *w, double *z,
+                                        bool intercept, double &alpha, ull n_vars, ull n_obs);
 
 double identity (double y)
 {
@@ -21,10 +23,6 @@ static double max(double a, double b)
     return a;
   return b;
 }
-
-static inline double
-compute_lambda_max(Rcpp::NumericMatrix X, double *w, double *z, bool intercept,
-									 double &alpha, ull n_vars, ull n_obs);
 
 /* fit_cpp: Fit a censored data distribution with elastic net reg.
  *
@@ -55,9 +53,9 @@ Rcpp::List fit_cpp(Rcpp::NumericMatrix X, Rcpp::NumericMatrix y,
   ull n_obs, n_vars;
   IREG_DIST orig_dist, transformed_dist;  // Orig dist is the one that is initially given
                                           // transformed_dist will be the dist of transformed output variables.
-																					// Eg- orig_dist = "loglogistic", transformed_dist="logistic", with transform_y=log
+                                          // Eg- orig_dist = "loglogistic", transformed_dist="logistic", with transform_y=log
   double (*transform_y) (double y);
-	double scale;
+  double scale;
 
   n_obs  = X.nrow();
   n_vars = X.ncol();  // n_vars is the number of variables corresponding to the coeffs of X
@@ -117,20 +115,20 @@ Rcpp::List fit_cpp(Rcpp::NumericMatrix X, Rcpp::NumericMatrix y,
   }
 
   /* Create output variables */
-	if (lambda_path.size() > 0) {
-		num_lambda = lambda_path.size();
-	}
+  if (lambda_path.size() > 0) {
+    num_lambda = lambda_path.size();
+  }
   Rcpp::NumericMatrix out_beta(n_vars, num_lambda);       // will contain the entire series of solutions
   Rcpp::IntegerVector out_n_iters(num_lambda);
   Rcpp::NumericVector out_lambda(num_lambda);
   Rcpp::NumericVector out_scale(num_lambda);
   Rcpp::NumericVector out_loglik(num_lambda);
 
-	/* use given values for the lambda path */
-	if (lambda_path.size() > 0) {
-		for(ull i = 0; i < num_lambda; ++i)
-			out_lambda[i] = lambda_path[i];
-	}
+  /* use given values for the lambda path */
+  if (lambda_path.size() > 0) {
+    for(ull i = 0; i < num_lambda; ++i)
+      out_lambda[i] = lambda_path[i];
+  }
 
   double *beta;                           // Initially points to the first solution
   int *n_iters = INTEGER(out_n_iters);
@@ -146,8 +144,8 @@ Rcpp::List fit_cpp(Rcpp::NumericMatrix X, Rcpp::NumericMatrix y,
   double *w  = new double [n_obs];    // diagonal of the hessian of LL wrt eta
                                       // these are the weights of the IRLS linear reg
   double *z = new double [n_obs];     // z_i = eta_i - mu_i / w_i
-	double *mu = new double [n_obs + 1];
-	double *ym = new double [n_obs];		// the mean values for y's
+  double *mu = new double [n_obs + 1];
+  double *ym = new double [n_obs];    // the mean values for y's
   IREG_CENSORING *status;
 
 
@@ -161,21 +159,21 @@ Rcpp::List fit_cpp(Rcpp::NumericMatrix X, Rcpp::NumericMatrix y,
   }
   get_y_means(y, status, ym);
 
-	/* SCALE RULES:
-	 * Whether or not it is estimated depends on estimate_scale. For exponential, this is forced to False and scale fixed to 1. // TODO
-	 *
-	 * If you provide no scale, a starting value will be calculated
-	 * If you provide a scale, it will be used as the initial value
-	 */
+  /* SCALE RULES:
+   * Whether or not it is estimated depends on estimate_scale. For exponential, this is forced to False and scale fixed to 1. // TODO
+   *
+   * If you provide no scale, a starting value will be calculated
+   * If you provide a scale, it will be used as the initial value
+   */
   if (scale_init == Rcpp::NA) {
     scale = get_init_var(ym, status, n_obs, transformed_dist);
-		// scale = sqrt(scale);		// use 2 * sqrt(var) as in survival
-		scale = 2 * sqrt(scale);		// use 2 * sqrt(var) as in survival
-		// TODO: find corner cases where you need to take 2 * sqrt(var) as in survival
-	} else {
-		scale = scale_init;
-	}
-	log_scale = log(scale);
+    // scale = sqrt(scale);    // use 2 * sqrt(var) as in survival
+    scale = 2 * sqrt(scale);    // use 2 * sqrt(var) as in survival
+    // TODO: find corner cases where you need to take 2 * sqrt(var) as in survival
+  } else {
+    scale = scale_init;
+  }
+  log_scale = log(scale);
 
   /* Initalize the pathwise solution
    * We will always start with lambda_max, at which beta = 0, eta = 0.
@@ -183,7 +181,7 @@ Rcpp::List fit_cpp(Rcpp::NumericMatrix X, Rcpp::NumericMatrix y,
    */
 
   // set beta = 0 and eta = 0
-	beta = REAL(out_beta);
+  beta = REAL(out_beta);
   for (ull i = 0; i < n_vars; ++i) {
     // sets only the first solution (first col of out_beta) to 0
     beta[i] = 0;
@@ -194,56 +192,56 @@ Rcpp::List fit_cpp(Rcpp::NumericMatrix X, Rcpp::NumericMatrix y,
     eta[i] = w[i] = z[i] = 0;
   }
 
-	/******************************************************************************************/
+  /******************************************************************************************/
   /* Iterate over grid of lambda values */
   bool flag_beta_converged = 0;
   double sol_num, sol_denom;
   double beta_new;
   double old_scale;
-	double lambda_max_unscaled;
+  double lambda_max_unscaled;
   double eps_ratio = std::pow(eps_lambda, 1.0 / (num_lambda-1));
 
   for (int m = 0; m < num_lambda; ++m) {
-		/* Compute the lambda path */
-		if (lambda_path.size() == 0) {
+    /* Compute the lambda path */
+    if (lambda_path.size() == 0) {
 
-			/* Do an initial fit with lambda set to BIG, will fit scale and intercept if applicable */
-			if (m == 0) {
-				lambda_max_unscaled = lambda_seq[0] = 1e35;
-			}
+      /* Do an initial fit with lambda set to BIG, will fit scale and intercept if applicable */
+      if (m == 0) {
+        lambda_max_unscaled = lambda_seq[0] = 1e35;
+      }
 
-			/* Calculate lambda_max using intial scale fit */
-			if (m == 1) {
-				lambda_seq[m] = compute_lambda_max(X, w, z, intercept, alpha, n_vars, n_obs);
-				lambda_max_unscaled = lambda_seq[m] * scale * scale;
-				// lambda_max_unscaled = lambda_seq[m] = lambda_seq[m] * scale * scale;
+      /* Calculate lambda_max using intial scale fit */
+      if (m == 1) {
+        lambda_seq[m] = compute_lambda_max(X, w, z, intercept, alpha, n_vars, n_obs);
+        lambda_max_unscaled = lambda_seq[m] * scale * scale;
+        // lambda_max_unscaled = lambda_seq[m] = lambda_seq[m] * scale * scale;
 
-			/* Last solution should be unregularized if the flag is set */
-			} else if (m == num_lambda - 1 && unreg_sol == true)
-    	  lambda_seq[m] = 0;
+      /* Last solution should be unregularized if the flag is set */
+      } else if (m == num_lambda - 1 && unreg_sol == true)
+        lambda_seq[m] = 0;
 
-			/* All other lambda calculated */
+      /* All other lambda calculated */
       else if (m > 1) {
         // lambda_seq[m] = lambda_seq[m - 1] * eps_ratio;
         lambda_seq[m] = lambda_max_unscaled * pow(eps_ratio, m-1) / scale / scale;
       }
-		}
+    }
 
     /* Initialize the solution at this lambda using previous lambda solution */
     // We need to explicitly do this because we need to store all the solutions separately
-		if (m != 0) {
-			for (ull i = 0; i < n_vars; ++i) {
-				beta[i + n_vars] = beta[i];
-			}
-			beta = beta + n_vars;   // go to the next column
-		}
+    if (m != 0) {
+      for (ull i = 0; i < n_vars; ++i) {
+        beta[i + n_vars] = beta[i];
+      }
+      beta = beta + n_vars;   // go to the next column
+    }
 
     /* CYCLIC COORDINATE DESCENT: Repeat until convergence of beta */
     n_iters[m] = 0;
     do {                                  // until Convergence of beta
 
       flag_beta_converged = 1;            // = 1 if beta converges
-			old_scale = scale;
+      old_scale = scale;
 
       // IRLS: Reweighting step: calculate w and z again (beta & hence eta would have changed)  TODO: make dg ddg, local so that we can save computations?
       compute_grad_response(w, z, &scale_update, REAL(y), REAL(y) + n_obs, eta, scale,     // TODO:store a ptr to y?
@@ -284,13 +282,13 @@ Rcpp::List fit_cpp(Rcpp::NumericMatrix X, Rcpp::NumericMatrix y,
 
       if (estimate_scale) {
         log_scale += scale_update; scale = exp(log_scale);
-				// scale the lambda value according to current scale unless you are at unregularized sol
-				// done to match values with Glment for Gaussian, no censoring
+        // scale the lambda value according to current scale unless you are at unregularized sol
+        // done to match values with Glment for Gaussian, no censoring
         if ((m != num_lambda - 1 || unreg_sol == false) && lambda_path.size() == 0 && m > 1)
           lambda_seq[m] = lambda_max_unscaled * pow(eps_ratio, m-1) / scale / scale;    // FIXME: Scale! :O
 
-        // if (fabs(scale - old_scale) > threshold) {		// TODO: Maybe should be different for sigma?
-        if (fabs(scale - old_scale) > 1e-4) {		// TODO: Maybe should be different for sigma?
+        // if (fabs(scale - old_scale) > threshold) {    // TODO: Maybe should be different for sigma?
+        if (fabs(scale - old_scale) > 1e-4) {    // TODO: Maybe should be different for sigma?
           flag_beta_converged = 0;
         }
       }
@@ -299,15 +297,15 @@ Rcpp::List fit_cpp(Rcpp::NumericMatrix X, Rcpp::NumericMatrix y,
     } while ((flag_beta_converged != 1) && (n_iters[m] < max_iter));
 
     out_scale[m] = scale;
-		if (m == 0)
-			scale_init = scale;
+    if (m == 0)
+      scale_init = scale;
   } // end for: lambda
 
   /* Scale the coefs back to the original scale */
   if (flag_standardize_x) {
     for (ull m = 0; m < num_lambda; ++m) {
 
-      for (ull i = int(intercept); i < n_vars; ++i) {	// no standardization for intercept
+      for (ull i = int(intercept); i < n_vars; ++i) {  // no standardization for intercept
         out_beta(i, m) = out_beta(i, m) / std_x[i];
       }
     }
@@ -344,16 +342,16 @@ void get_y_means (Rcpp::NumericMatrix &y, IREG_CENSORING *status, double *ym)
         case IREG_CENSOR_LEFT:
         case IREG_CENSOR_RIGHT:
         case IREG_CENSOR_NONE:
-				  ym[i] = y(i, 0);
+          ym[i] = y(i, 0);
           break;
 
         case IREG_CENSOR_INTERVAL:
-				  ym[i] = (y(i, 1) + y(i, 0)) / 2;
+          ym[i] = (y(i, 1) + y(i, 0)) / 2;
           break;
 
         default:
           ym[i] = 0;
-			}
+      }
       // std::cout<<"status, ym: " << status[i] << " " << ym[i] << "\n";
   } // end for
 }
@@ -376,7 +374,7 @@ void get_censoring_types (Rcpp::NumericMatrix &y, IREG_CENSORING *status)
       status[i] = IREG_CENSOR_RIGHT;
     else if (y(i, 0) == y(i, 1))
       status[i] = IREG_CENSOR_NONE;         // no censoring
-		else
+    else
       status[i] = IREG_CENSOR_INTERVAL;     // interval censoring
 
   } // end for
@@ -413,7 +411,7 @@ static void standardize_x (Rcpp::NumericMatrix X,
 {
   double temp;
 
-  for (ull i = int(intercept); i < X.ncol(); ++i) {	// don't standardize intercept col.
+  for (ull i = int(intercept); i < X.ncol(); ++i) {  // don't standardize intercept col.
     mean_x[i] = std_x[i] = 0;
     for (ull j = 0; j < X.nrow(); ++j) {
       mean_x[i] += X(j, i);
@@ -436,62 +434,62 @@ static void standardize_x (Rcpp::NumericMatrix X,
 
 static double get_init_var (double *ym, IREG_CENSORING *status, ull n, IREG_DIST dist)
 {
-	double mean = 0, var = 0;
+  double mean = 0, var = 0;
 
-	for (int i = 0; i < n; ++i) {
-		mean += ym[i];
-	}
-	mean = mean / n;
+  for (int i = 0; i < n; ++i) {
+    mean += ym[i];
+  }
+  mean = mean / n;
 
-	for (int i = 0; i < n; ++i) {
-		var += pow((ym[i] - mean), 2);
-	}
-	var = var / n;
+  for (int i = 0; i < n; ++i) {
+    var += pow((ym[i] - mean), 2);
+  }
+  var = var / n;
 
-	switch (dist) {
-		case IREG_DIST_EXTREME_VALUE :
-			mean = mean + 0.572;
-			var = var / 1.64;
-		break;
+  switch (dist) {
+    case IREG_DIST_EXTREME_VALUE :
+      mean = mean + 0.572;
+      var = var / 1.64;
+    break;
 
     case IREG_DIST_LOGISTIC:
-			var = var / 3.2;
-		break;
+      var = var / 3.2;
+    break;
 
-		default:
-		break;
-	}
+    default:
+    break;
+  }
 
-	return var;
+  return var;
 }
 
 static double get_init_intercept (double *mu, double *w, double *ym, ull n_obs)
 {
-	double intercept = 0;
+  double intercept = 0;
 
-	for (ull i = 0; i < n_obs; ++i) {
-		intercept += (mu[i] - w[i] * ym[i]);
-	}
-	intercept /= n_obs;
+  for (ull i = 0; i < n_obs; ++i) {
+    intercept += (mu[i] - w[i] * ym[i]);
+  }
+  intercept /= n_obs;
 
-	return intercept;
+  return intercept;
 }
 
 static inline double
 compute_lambda_max(Rcpp::NumericMatrix X, double *w, double *z, bool intercept,
-									 double &alpha, ull n_vars, ull n_obs)
+                   double &alpha, ull n_vars, ull n_obs)
 {
-	double lambda_max = -1; // calculated values are always non-negative
-	for (ull j = int(intercept); j < n_vars; ++j) {   // dont include intercept col in lambda calc.
-	  double temp = 0;
+  double lambda_max = -1; // calculated values are always non-negative
+  for (ull j = int(intercept); j < n_vars; ++j) {   // dont include intercept col in lambda calc.
+    double temp = 0;
 
-	  for (ull i = 0; i < n_obs; ++i) {
-	    temp += (w[i] * X(i, j) * z[i]);
-	  }
-		temp = fabs(temp);
-	  lambda_max = (lambda_max > temp)? lambda_max: temp;
-	}
-	lambda_max /= (n_obs * max(alpha, 1e-3));  // prevent divide by zero
+    for (ull i = 0; i < n_obs; ++i) {
+      temp += (w[i] * X(i, j) * z[i]);
+    }
+    temp = fabs(temp);
+    lambda_max = (lambda_max > temp)? lambda_max: temp;
+  }
+  lambda_max /= (n_obs * max(alpha, 1e-3));  // prevent divide by zero
 
-	return lambda_max;
+  return lambda_max;
 }
