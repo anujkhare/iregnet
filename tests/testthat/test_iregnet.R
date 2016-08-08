6097267ecfb66aa392b088e9412dcac86b014015
Anuj Khare <khareanuj18@gmail.com>
2016-08-08 12:14:51 +0530

    Tests for validation of y, change in error msg

--- tests/testthat/test_iregnet.R
+++ tests/testthat/test_iregnet.R
@@ -4,6 +4,30 @@ library("glmnet")
 
 # TODO: You should check if the suggested packages are present, and only then use them for testing
 
+test_that("Output y is validated properly", {
+  data("ovarian")
+  x <- cbind(ovarian$ecog.ps, ovarian$rx)
+  y_l <- ovarian$futime
+  y_r <- ovarian$futime
+  y_r[ovarian$fustat == 0] <- NA
+
+  expect_error(iregnet(x, 10), "y should be a 2 column matrix, or a Surv object")
+  # 2 column matrix
+  expect_error(iregnet(x, cbind(y_l, y_r-1)), "Invalid interval*")
+  expect_error(iregnet(x, cbind(y_r, y_r)), "Invalid interval*")
+  expect_error(iregnet(x, cbind(y_l, y_l, y_l)), "y should be a 2 column matrix")
+
+  # Surv object
+  expect_error(iregnet(x, Surv(y_l, ovarian$fustat, type="mstate")), "Unsupported censoring type from Surv")
+
+  # Size wrt X
+  expect_error(iregnet(x, cbind(y_l[1:2], y_r[1:2])), "*")
+  expect_error(iregnet(x, Surv(y_l[1:2], y_r[1:2], type="interval2")), "*")
+
+  # postivity
+  expect_error(iregnet(x, cbind(-y_l, y_r), family="loglogistic"), "y should be positive for the given family")
+});
+
 std <- F
 get_xy <- function(n_obs, n_vars, type = c('right', 'left', 'none', 'interval'), standardize=std, positive=F) {
   type <- match.arg(type)
