917065aee79078753d8effe6bbba65200a7379b5
Toby Dylan Hocking <tdhock5@gmail.com>
2017-04-05 15:47:03 -0400

    export predict

--- R/cv.R
+++ R/cv.R
@@ -242,6 +242,17 @@ plot.cv.iregnet <- function(x, ...){
   })
 }
 
+##' Predict CV iregnet
+##'
+##' Get predicted values
+##' @title predict CV iregnet
+##' @param object result of cv.iregnet
+##' @param newx feature matrix
+##' @param type min or 1sd
+##' @param ... 
+##' @return predicted values
+##' @export
+##' @author Toby Dylan Hocking
 predict.cv.iregnet <- function(object, newx, type="min", ...){
   i <- object$selected[[type]]
   not.cv <- object
