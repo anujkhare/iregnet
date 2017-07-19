9f7c158dde35d44871c07bbcf4bf9d6cd6dcaee2
Rover <atrovervan@gmail.com>
2017-07-19 23:18:30 +0800

    Sort matrix X & y outside the loop (support right censoring type)

--- src/distributions_gaussian.cpp
+++ src/distributions_gaussian.cpp
@@ -18,6 +18,7 @@
 #include "R.h"
 #include "iregnet.h"
 
+
 #define SPI 2.506628274631001     /* sqrt(2*pi) */
 #define ROOT_2 1.414213562373095
 #define SMALL -200   /* what to use for log(f(x)) if f(x) is zero */
@@ -25,9 +26,9 @@
 
 double
 compute_grad_response_gaussian_none(rowvec *w, rowvec *z, double *scale_update, const rowvec *y_l, const rowvec *y_r,
-                                    const rowvec *eta, const double scale, const IREG_CENSORING *censoring_type,
+                                    const rowvec &eta, const double scale, const IREG_CENSORING *censoring_type,
                                     const ull n_obs, IREG_DIST dist, double *mu, bool debug, const bool estimate_scale,
-                                    rowvec *y_eta, rowvec *y_eta_square) {
+                                    rowvec &y_eta, rowvec &y_eta_square, int *separator, rowvec *tempvar) {
   double scale_2 = scale * scale;
   double loglik;
   double dsig_sum, ddsig_sum;
@@ -38,15 +39,15 @@ compute_grad_response_gaussian_none(rowvec *w, rowvec *z, double *scale_update, 
   loglik = 0;
   dsig_sum = ddsig_sum = 0;
 
-  loglik = accu((*y_eta_square) / -2) - logspispecial;
-  (*z) = (*eta) + scale * (*y_eta);
+  loglik = accu(y_eta_square / -2) - logspispecial;
+  (*z) = eta + scale * y_eta;
   (*w).fill(-1 / scale_2);
 
   if (scale_update && estimate_scale == 1) {
-    dsig_vec = (*y_eta_square);//m
+    dsig_vec = y_eta_square;//m
     /*ddsig_vec_one = (square(y_eta_one_square) - y_eta_one_square) / (scale_2 * scale_2)
                           - dsig_vec_one % (dsig_vec_one + 1);*/
-    ddsig_vec = -2 * (*y_eta_square);
+    ddsig_vec = -2 * y_eta_square;
     // dsg = sz * temp2 - dg*(dsig +1);
     dsig_vec -= 1;
     dsig_sum += accu(dsig_vec);
@@ -63,11 +64,10 @@ compute_grad_response_gaussian_none(rowvec *w, rowvec *z, double *scale_update, 
 
 double
 compute_grad_response_gaussian_right(rowvec *w, rowvec *z, double *scale_update, const rowvec *y_l, const rowvec *y_r,
-                                     const rowvec *eta, const double scale, const IREG_CENSORING *censoring_type,
+                                     const rowvec &eta, const double scale, const IREG_CENSORING *censoring_type,
                                      const ull n_obs, IREG_DIST dist, double *mu, bool debug, const bool estimate_scale,
-                                     rowvec *y_eta, rowvec *y_eta_square)
+                                     rowvec &y_eta, rowvec &y_eta_square, int *separator, rowvec *tempvar)
 {
-
   double dsig_sum = 0, ddsig_sum = 0;
   double loglik, scale_2 = scale * scale;
   loglik = 0;
@@ -76,68 +76,34 @@ compute_grad_response_gaussian_right(rowvec *w, rowvec *z, double *scale_update,
    * so that we can compute them by quickly way in armadillo
    * there are some temp vars below in this step
    */
-  rowvec eta_one(n_obs);
-  rowvec eta_two(n_obs);
 
-  rowvec y_eta_one(n_obs);
-  rowvec y_eta_one_square(n_obs);
-  rowvec y_eta_two(n_obs);
-  rowvec y_eta_two_square(n_obs);
+  ull none_censoring_type_number = separator[0];
+  ull right_censoring_type_number = n_obs - separator[0];
 
-  double count_one = 0;
-  double count_two = 0;
+  tempvar[0] = eta.subvec(0, separator[0] - 1); // eta_one
+  tempvar[1] = eta.subvec(separator[0], n_obs - 1);// eta_two
 
-  /* Separate eta[] and y_eta[] into two different type by their censoring type
-   * one means none censoring type
-   * two means right censoring type
-   */
-  for (int i = 0; i < n_obs; ++i) {
-    if(censoring_type[i] == 1){
-      y_eta_one(count_one) = (*y_eta)(i);
-      y_eta_one_square(count_one) = (*y_eta_square)(i);
-      eta_one(count_one) = (*eta)(i);
-      count_one ++;
-    } else if(censoring_type[i] == 0){
-      y_eta_two(count_two) = (*y_eta)(i);
-      y_eta_two_square(count_two) = (*y_eta_square)(i);
-      eta_two(count_two) = (*eta)(i);
-      count_two ++;
-    }
-  }
+  tempvar[2] = y_eta.subvec(0, separator[0] - 1); // y_eta_one
+  tempvar[3] = y_eta_square.subvec(0, separator[0] - 1); // y_eta_one_square
 
-  y_eta_one.resize(count_one);
-  y_eta_one_square.resize(count_one);
-  eta_one.resize(count_one);
-
-  y_eta_two.resize(count_two);
-  y_eta_two_square.resize(count_two);
-  eta_two.resize(count_two);
-
-  rowvec res_z_one(count_one);
-  rowvec res_z_two(count_two);
-  rowvec res_w_one(count_one);
-  rowvec res_w_two(count_two);
-
-  rowvec dsig_vec_one(count_one);
-  rowvec dsig_vec_two(count_two);
-  rowvec ddsig_vec_one(count_one);
-  rowvec ddsig_vec_two(count_two);
+  tempvar[4] = y_eta.subvec(separator[0], n_obs - 1); // y_eta_two
+  tempvar[5] = y_eta_square.subvec(separator[0], n_obs - 1); // y_eta_two_square
 
   //for none censoring type
-  loglik += accu(y_eta_one_square / -2) - count_one * log(SPI * scale);
-  res_z_one = eta_one + scale * y_eta_one;
-  res_w_one.fill(-1 / scale_2);
+  loglik += accu(tempvar[3] / -2) - none_censoring_type_number * log(SPI * scale);
+  tempvar[6] = tempvar[0] + scale * tempvar[2];
+  tempvar[8].fill(-1 / scale_2);
 
   if (scale_update) {
-    dsig_vec_one = y_eta_one_square;//m
-    /*ddsig_vec_one = (square(y_eta_one_square) - y_eta_one_square) / (scale_2 * scale_2)
-                          - dsig_vec_one % (dsig_vec_one + 1);*/
-    ddsig_vec_one = -2 * y_eta_one_square;
+    tempvar[10] = tempvar[3];//m
+    /*tempvar[12] = (square(tempvar[3]) - tempvar[3]) / (scale_2 * scale_2)
+                          - tempvar[10] % (tempvar[10] + 1);*/
+    tempvar[12] = -2 * tempvar[3];
     // dsg = sz * temp2 - dg*(dsig +1);
 
-    dsig_vec_one -= 1;
-    dsig_sum += accu(dsig_vec_one);
-    ddsig_sum += accu(ddsig_vec_one);
+    tempvar[10] -= 1;
+    dsig_sum += accu(tempvar[10]);
+    ddsig_sum += accu(tempvar[12]);
     /*if(m == 0 && n ==1) {
       std::cout << "dsig_sum :" << dsig_sum << "\n";
       std::cout << "ddsig_sum : " << ddsig_sum << "\n";
@@ -147,17 +113,11 @@ compute_grad_response_gaussian_right(rowvec *w, rowvec *z, double *scale_update,
 
   }
 
-  //for right censoring type
-  rowvec temp_densities(count_two);
-  rowvec dg_vec(count_two);
-  rowvec ddg_vec(count_two);
-  rowvec f_vec(count_two);
+  tempvar[17] = exp(-tempvar[5] / 2) /SPI;
 
-  f_vec = exp(-y_eta_two_square / 2) /SPI;
+  tempvar[14] = tempvar[4];
 
-  temp_densities = y_eta_two;
-
-  temp_densities.for_each( [](vec::elem_type& val) {
+  tempvar[14].for_each( [](vec::elem_type& val) {
       if (val > 0) {
         //          densities_l[0] = (1 + erf(y_eta(i)/ROOT_2))/2;
         val =  erfc(val /ROOT_2) /2;
@@ -178,13 +138,11 @@ compute_grad_response_gaussian_right(rowvec *w, rowvec *z, double *scale_update,
     }*/
   /* densities_l[2] = f;
    densities_l[3] = -y_eta(i)*f;*/
+  loglik += accu(tempvar[5] / -2) - right_censoring_type_number * log(SPI);
 
-  loglik += accu(y_eta_two_square / -2) - count_two * log(SPI);
-
-  dg_vec = (f_vec / temp_densities) / scale;
-  ddg_vec = ((y_eta_two % f_vec) / temp_densities) / scale_2;
-  ddg_vec -= square(dg_vec);
-
+  tempvar[15] = (tempvar[17] / tempvar[14]) / scale;
+  tempvar[16] = ((tempvar[4] % tempvar[17]) / tempvar[14]) / scale_2;
+  tempvar[16] -= square(tempvar[15]);
 /*
   temp  = -densities_l[2] / densities_l[1] / scale;
   temp2 = -densities_l[3] / densities_l[1] / scale_2;
@@ -192,33 +150,21 @@ compute_grad_response_gaussian_right(rowvec *w, rowvec *z, double *scale_update,
   ddg = temp2 - dg * dg;     // f'(z^l) / f()] / [1-F()] ...*/
 
   if (scale_update) {
-    dsig_vec_two = (f_vec % y_eta_two) / temp_densities;
-    //ddsig_vec_two = sz * sz* temp2 - dsig * (1 + dsig);
-    ddsig_vec_two = f_vec % y_eta_two;
-    ddsig_vec_two = (temp_densities % (y_eta_two_square - 1) - ddsig_vec_two) % ddsig_vec_two;
-    ddsig_vec_two = ddsig_vec_two / square(temp_densities);
+    tempvar[11] = (tempvar[17] % tempvar[4]) / tempvar[14];
+    //tempvar[13] = sz * sz* temp2 - dsig * (1 + dsig);
+    tempvar[13] = tempvar[17] % tempvar[4];
+    tempvar[13] = (tempvar[14] % (tempvar[5] - 1) - tempvar[13]) % tempvar[13];
+    tempvar[13] = tempvar[13] / square(tempvar[14]);
 
-    dsig_sum += accu(dsig_vec_two);
-    ddsig_sum += accu(ddsig_vec_two);
+    dsig_sum += accu(tempvar[11]);
+    ddsig_sum += accu(tempvar[13]);
   }
 
-  res_z_two = eta_two - dg_vec / ddg_vec;
-  res_w_two = ddg_vec;
+  tempvar[7] = tempvar[1] - tempvar[15] / tempvar[16];
+  tempvar[9] = tempvar[16];
 
-  count_one = 0;
-  count_two = 0;
-
-  for (int i = 0; i < n_obs; ++i) {
-    if(censoring_type[i] == 1){
-      (*z)(i) = res_z_one(count_one);
-      (*w)(i) = res_w_one(count_one);
-      count_one ++;
-    } else if(censoring_type[i] == 0){
-      (*z)(i) = res_z_two(count_two);
-      (*w)(i) = res_w_two(count_two);
-      count_two ++;
-    }
-  }
+  (*z) = join_rows(tempvar[6], tempvar[7]);
+  (*w) = join_rows(tempvar[8], tempvar[9]);
 
   if (ddsig_sum != 0)
     *scale_update = -dsig_sum / ddsig_sum;
@@ -230,9 +176,9 @@ compute_grad_response_gaussian_right(rowvec *w, rowvec *z, double *scale_update,
 
 double
 compute_grad_response_gaussian_left(rowvec *w, rowvec *z, double *scale_update, const rowvec *y_l, const rowvec *y_r,
-                                    const rowvec *eta, const double scale, const IREG_CENSORING *censoring_type,
+                                    const rowvec &eta, const double scale, const IREG_CENSORING *censoring_type,
                                     const ull n_obs, IREG_DIST dist, double *mu, bool debug, const bool estimate_scale,
-                                    rowvec *y_eta, rowvec *y_eta_square)
+                                    rowvec &y_eta, rowvec &y_eta_square, int *separator, rowvec *tempvar)
 {
 
   double dsig_sum = 0, ddsig_sum = 0;
@@ -252,14 +198,14 @@ compute_grad_response_gaussian_left(rowvec *w, rowvec *z, double *scale_update, 
 
   for (int i = 0; i < n_obs; ++i) {
     if(censoring_type[i] == 1){
-      y_eta_one(count_one) = (*y_eta)(i);
-      y_eta_one_square(count_one) = (*y_eta_square)(i);
-      eta_one(count_one) = (*eta)(i);
+      y_eta_one(count_one) = y_eta(i);
+      y_eta_one_square(count_one) = y_eta_square(i);
+      eta_one(count_one) = eta(i);
       count_one ++;
     } else if(censoring_type[i] == 2){
-      y_eta_two(count_two) = (*y_eta)(i);
-      y_eta_two_square(count_two) = (*y_eta_square)(i);
-      eta_two(count_two) = (*eta)(i);
+      y_eta_two(count_two) = y_eta(i);
+      y_eta_two_square(count_two) = y_eta_square(i);
+      eta_two(count_two) = eta(i);
       count_two ++;
     }
   }
