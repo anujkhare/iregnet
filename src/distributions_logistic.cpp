e4cfc7cbad8e11ba9073ca44c0f55a4ab84ff6f5
Rover <atrovervan@gmail.com>
2017-08-07 11:29:49 +0800

    Add right censoring type function of logistic distribution

--- src/distributions_logistic.cpp
+++ src/distributions_logistic.cpp
@@ -31,6 +31,10 @@ compute_only_none_censoring_type_logistic(rowvec *w, rowvec *z, double *scale_up
                                  const bool estimate_scale, const ull n_obs);
 
 double
+compute_left_or_right_censoring_type_logistic(rowvec *w, rowvec *z, double *scale_update, const double scale, const rowvec &eta,
+                                              const rowvec &y_eta, const rowvec &y_eta_square, double &dsig_sum, double &ddsig_sum,
+                                              const bool estimate_scale, const ull n_obs, const bool type);
+double
 compute_grad_response_logistic_none(rowvec *w, rowvec *z, double *scale_update, const rowvec *y_l, const rowvec *y_r,
                                     const rowvec &eta, const double scale, const IREG_CENSORING *censoring_type,
                                     const ull n_obs, IREG_DIST dist, double *mu, bool debug, const bool estimate_scale,
@@ -41,6 +45,55 @@ compute_grad_response_logistic_none(rowvec *w, rowvec *z, double *scale_update, 
 
   loglik = compute_only_none_censoring_type_logistic(w, z, scale_update, scale, eta, y_eta, y_eta_square, dsig_sum, ddsig_sum,
                                             estimate_scale, n_obs);
+
+  if (scale_update && estimate_scale == 1) {
+    if (ddsig_sum != 0)
+      *scale_update = -dsig_sum / ddsig_sum;
+    else
+      *scale_update = BIG_SIGMA_UPDATE;
+  }
+
+  return loglik;
+}
+
+double
+compute_grad_response_logistic_right(rowvec *w, rowvec *z, double *scale_update, const rowvec *y_l, const rowvec *y_r,
+                                     const rowvec &eta, const double scale, const IREG_CENSORING *censoring_type,
+                                     const ull n_obs, IREG_DIST dist, double *mu, bool debug, const bool estimate_scale,
+                                     const rowvec &y_eta, const rowvec &y_eta_square,const int *separator, rowvec *tempvar)
+{
+  double loglik = 0;
+  double dsig_sum, ddsig_sum;
+  dsig_sum = ddsig_sum = 0;
+
+  ull none_censoring_type_number = separator[0];
+  ull right_censoring_type_number = n_obs - separator[0];
+
+  tempvar[0] = eta.subvec(0, separator[0] - 1); // eta_one
+  tempvar[1] = eta.subvec(separator[0], n_obs - 1);// eta_two
+
+  tempvar[2] = y_eta.subvec(0, separator[0] - 1); // y_eta_one
+  tempvar[3] = y_eta_square.subvec(0, separator[0] - 1); // y_eta_one_square
+
+  tempvar[4] = y_eta.subvec(separator[0], n_obs - 1); // y_eta_two
+  tempvar[5] = y_eta_square.subvec(separator[0], n_obs - 1); // y_eta_two_square
+
+  tempvar[6] = rowvec(none_censoring_type_number); // res_z_one
+  tempvar[7] = rowvec(right_censoring_type_number); // res_z_two
+  tempvar[8] = rowvec(none_censoring_type_number); // res_w_one
+  tempvar[9] = rowvec(right_censoring_type_number); // res_w_two
+
+  // compute for none
+  loglik = compute_only_none_censoring_type_logistic(&tempvar[8], &tempvar[6], scale_update, scale, tempvar[0], tempvar[2],
+                                            tempvar[3], dsig_sum, ddsig_sum, estimate_scale, none_censoring_type_number);
+
+  // compute for right
+  loglik += compute_left_or_right_censoring_type_logistic(&tempvar[9], &tempvar[7], scale_update, scale, tempvar[1], tempvar[4],
+                                                 tempvar[5], dsig_sum, ddsig_sum, estimate_scale,
+                                                 right_censoring_type_number, true);
+
+  (*z) = join_rows(tempvar[6], tempvar[7]);
+  (*w) = join_rows(tempvar[8], tempvar[9]);
 
   if (scale_update && estimate_scale == 1) {
     if (ddsig_sum != 0)
@@ -100,4 +153,75 @@ compute_only_none_censoring_type_logistic(rowvec *w, rowvec *z, double *scale_up
   return loglik;
 }
 
+double
+compute_left_or_right_censoring_type_logistic(rowvec *w, rowvec *z, double *scale_update, const double scale, const rowvec &eta,
+                                     const rowvec &y_eta, const rowvec &y_eta_square, double &dsig_sum, double &ddsig_sum,
+                                     const bool estimate_scale, const ull n_obs, const bool type)
+{
+  double scale_2 = scale * scale;
+  double loglik = 0;
+  rowvec dsig_vec;
+  rowvec ddsig_vec;
+  //double w_base;
+  double w_1;
+
+  rowvec temp_densities(n_obs);
+  rowvec dg_vec(n_obs);
+  rowvec ddg_vec(n_obs);
+
+  rowvec temp_w_vec = rowvec(n_obs);
+  rowvec sign_vec = rowvec(n_obs);
+  rowvec sz_vec = scale * y_eta;
+  rowvec temp_1 = rowvec(n_obs);
+  rowvec temp_2 = rowvec(n_obs);
+  rowvec w_base = rowvec(n_obs);
+  rowvec w_base_square = rowvec(n_obs);
+  rowvec f_vec = rowvec(n_obs);
+
+  // if type is true, right censoring is computed
+  // else left censoring is computed
+
+  if(type){
+
+    for (int i = 0; i < n_obs; ++i) {
+      if(y_eta(i)>0){
+        w_base(i) = std::exp(-y_eta(i));
+        f_vec(i) = w_base(i) / (1 + w_base(i));
+        sign_vec(i) = -1;
+      } else {
+        w_base(i) = std::exp(y_eta(i));
+        f_vec(i) = 1 / (1 + w_base(i));
+        sign_vec(i) = 1;
+      }
+    }
+
+    w_base_square = square(1 + w_base);
+
+    temp_1 = -1 * ((w_base / w_base_square) / f_vec / scale);
+    temp_2 = sign_vec % w_base % (1 - w_base);
+    temp_2 = -1 * temp_2 / pow(1 + w_base, 3) / scale_2;
+
+    dg_vec = -1 * temp_1;
+    ddg_vec = temp_2 - square(dg_vec);
+
+    if (scale_update) {
+      dsig_vec = dg_vec % (scale * y_eta);
+      ddsig_vec = scale_2 * y_eta_square;
+      ddsig_vec = ddsig_vec % temp_2;
+      ddsig_vec = ddsig_vec - dsig_vec % (1 + dsig_vec);
+
+      dsig_sum += accu(dsig_vec);
+      ddsig_sum += accu(ddsig_vec);
+    }
+
+  } else {
+
+    //todo : support left censoring type
+  }
+
+  (*z) = eta - dg_vec / ddg_vec;
+  (*w) = ddg_vec;
+
+  return loglik;
+}
 
