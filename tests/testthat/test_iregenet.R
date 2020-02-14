97e52631fc83041581a0aef193f9eb60cd87112d
Andrew Hurst <andrew_hurst92@yahoo.com>
2020-02-14 01:28:18 -0700

    split data into train and test

--- tests/testthat/test_iregenet.R
+++ tests/testthat/test_iregenet.R
@@ -1,113 +1,124 @@
 context("iregnet prediction")
 library(iregnet)
 
+load("data/neuroblastoma.Rdata")
+### load("data/neuroblastoma.Rdata")
+samp.data <- cbind(neuroblastoma.data$inputs , neuroblastoma.data$outputs)
+samp.data <- matrix( samp.data , nrow(samp.data) , ncol(samp.data))
 
-testFold.dir.vec <- Sys.glob(file.path(
-  "data", "*", "cv", "*", "testFolds",
-  "*"))
+## randomize data
+samp.data <- samp.data[ sample(nrow(samp.data)),]
 
-n.pred.vec <- sapply(testFold.dir.vec, function(testFold.dir){
-  path <- file.path(dirname(testFold.dir), "models", "*", "predictions.csv")
-  length(Sys.glob(path))
-})
+#put into folds (5)
+fold.size <- nrow(samp.data) / 5
+samp.fold.1 <- samp.data[c(1: 93),]
+samp.fold.2 <- samp.data[c(94 : 186),]
+samp.fold.3 <- samp.data[c(187 : 279),]
+samp.fold.4 <- samp.data[c(280 : 372),]
+samp.fold.5 <- samp.data[c(373: 465),]
 
-testFold.dir <- testFold.dir.vec[[1]]
-data.m.list <- list()
+### input
+samp.fold.1.X <- samp.fold.1[, c(1 : 29)] 
+samp.fold.2.X <- samp.fold.2[, c(1 : 29)] 
+samp.fold.3.X <- samp.fold.3[, c(1 : 29)] 
+samp.fold.4.X <- samp.fold.4[, c(1 : 29)] 
+samp.fold.5.X <- samp.fold.5[, c(1 : 29)] 
 
-## format data
-
-FormatData <- function( testFold.dir ){
-  library(data.table)
-  test.fold <- as.integer(basename(testFold.dir))
-  cv.type.dir <- dirname(dirname(testFold.dir))
-  data.dir <- dirname(dirname(cv.type.dir))
-  folds.csv <- file.path(cv.type.dir, "folds.csv")
-  folds.dt <- fread(folds.csv)
-  data.list <- list()
-  for(data.type in c("inputs", "outputs")){
-    csv.xz <- file.path(data.dir, paste0(data.type, ".csv.xz"))
-    dt <- fread(cmd=paste("xzcat", csv.xz))
-    stopifnot(nrow(dt) == nrow(folds.dt))
-    m <- as.matrix(dt[, -1, with=FALSE])
-    rownames(m) <- dt$sequenceID
-    data.list[[data.type]] <- m
-  }
-  rep.val.vec <- c(
-    log.log.bases="log2.n",
-    n.loglog="log2.n",
-    "diff abs.identity.quantile.50%"="log.hall",
-    log.sd="log.hall")
-  for(old.name in names(rep.val.vec)){
-    new.name <- rep.val.vec[[old.name]]
-    colnames(data.list$inputs)[colnames(data.list$inputs) == old.name] <- new.name
-  }
-  keep.inputs <- apply(is.finite(data.list$inputs), 2, all)
-  data.list$inputs <- data.list$inputs[, keep.inputs, drop=FALSE]
-  id.list <- list(
-    train=folds.dt[fold != test.fold, sequenceID],
-    test=folds.dt[fold == test.fold, sequenceID])
-  set.list <- list()
-  for(set.name in names(id.list)){
-    set.id.vec <- id.list[[set.name]]
-    set.list[[set.name]] <- lapply(data.list, function(m){
-      m[set.id.vec,]
-    })
-  }
-  
-  X.train <- matrix(set.list$train$inputs, nrow(set.list$train$inputs), ncol(set.list$train$inputs))
-  Y.train <-  matrix( set.list$train$outputs , nrow(set.list$train$outputs) , ncol(set.list$train$outputs))
-  
-  data.vec <- list( X = X.train ,y = Y.train)
-  return(data.vec)
-}
+### output
+samp.fold.1.y <- samp.fold.1[, c(30 : 31)] 
+samp.fold.2.y <- samp.fold.2[, c(30 : 31)] 
+samp.fold.3.y <- samp.fold.3[, c(30 : 31)] 
+samp.fold.4.y <- samp.fold.4[, c(30 : 31)] 
+samp.fold.5.y <- samp.fold.5[, c(30 : 31)]
 
 ## test all dataset folds for error and place as test case
 
-## testFold.dir <- testFold.dir.vec[[1]]
-data.m.list <- FormatData(testFold.dir.vec[[1]])
-X.train <- data.m.list$X
-Y.train <- data.m.list$y
+ # test fold = 5
+X.input <- rbind(samp.fold.1.X , samp.fold.2.X)
+X.input <- rbind( X.input, samp.fold.3.X)
+X.input <- rbind( X.input, samp.fold.4.X)
 
+
+Y <- rbind(samp.fold.1.y , samp.fold.2.y)
+Y <- rbind(Y , samp.fold.3.y)
+Y <- rbind(Y , samp.fold.4.y)
+
+### Failed
+iregnet( X.input , Y , family = "gaussian" )
+
+
+
+# test fold = 4
+X.input <- rbind(samp.fold.1.X , samp.fold.2.X)
+X.input <- rbind( X.input, samp.fold.3.X)
+X.input <- rbind( X.input, samp.fold.5.X)
+
+
+Y <- rbind(samp.fold.1.y , samp.fold.2.y)
+Y <- rbind(Y , samp.fold.3.y)
+Y <- rbind(Y , samp.fold.5.y)
+
+### Failed
+iregnet( X.input , Y , family = "gaussian" )
+
+
+
+# test fold = 3
+X.input <- rbind(samp.fold.1.X , samp.fold.2.X)
+X.input <- rbind( X.input, samp.fold.4.X)
+X.input <- rbind( X.input, samp.fold.5.X)
+
+
+Y <- rbind(samp.fold.1.y , samp.fold.2.y)
+Y <- rbind(Y , samp.fold.4.y)
+Y <- rbind(Y , samp.fold.5.y)
+
+
+### Failed
+iregnet( X.input , Y , family = "gaussian" )
+
+
+
+# test fold = 2
+X.input <- rbind(samp.fold.1.X , samp.fold.3.X)
+X.input <- rbind( X.input, samp.fold.4.X)
+X.input <- rbind( X.input, samp.fold.5.X)
+
+
+Y <- rbind(samp.fold.1.y , samp.fold.3.y)
+Y <- rbind(Y , samp.fold.4.y)
+Y <- rbind(Y , samp.fold.5.y)
+
+### Failed
+iregnet( X.input , Y , family = "gaussian" )
+
+
+
+# test fold = 1
+X.input <- rbind(samp.fold.2.X , samp.fold.3.X)
+X.input <- rbind( X.input, samp.fold.4.X)
+X.input <- rbind( X.input, samp.fold.5.X)
+
+
+Y <- rbind(samp.fold.1.y , samp.fold.3.y)
+Y <- rbind(Y , samp.fold.4.y)
+Y <- rbind(Y , samp.fold.5.y)
+
+### Failed
+iregnet( X.input , Y , family = "gaussian" )
+
+
+
+
+###################
 
 set.seed(1)
 ### FAILED
-cv.iregnet(X.train , Y.train , family= "gaussian" , scale_init= NA , estimate_scale = TRUE)
-cv.iregnet(X.train , Y.train , family= "gaussian" , scale_init= 1 , estimate_scale = FALSE)
-cv.iregnet(X.train , Y.train , family= "logistic" , scale_init= NA , estimate_scale = TRUE)
-cv.iregnet(X.train , Y.train , family= "logistic" , scale_init= 1 , estimate_scale = FALSE)
+iregnet(X.train , Y.train , family= "gaussian" , scale_init= NA , estimate_scale = TRUE)
+iregnet(X.train , Y.train , family= "gaussian" , scale_init= 1 , estimate_scale = FALSE)
+iregnet(X.train , Y.train , family= "logistic" , scale_init= NA , estimate_scale = TRUE)
+iregnet(X.train , Y.train , family= "logistic" , scale_init= 1 , estimate_scale = FALSE)
 
-### PASSED
-#none
-
-
-## testFold.dir <- testFold.dir.vec[[2]]
-data.m.list <- FormatData(testFold.dir.vec[[2]])
-X.train <- data.m.list$X
-Y.train <- data.m.list$y
-
-set.seed(1)
-### FAILED
-cv.iregnet(X.train , Y.train , family= "gaussian" , scale_init= NA , estimate_scale = TRUE)
-cv.iregnet(X.train , Y.train , family= "gaussian" , scale_init= 1 , estimate_scale = FALSE)
-cv.iregnet(X.train , Y.train , family= "logistic" , scale_init= NA , estimate_scale = TRUE)
-cv.iregnet(X.train , Y.train , family= "logistic" , scale_init= 1 , estimate_scale = FALSE)
-
-### PASSED
-
-
-## testFold.dir <- testFold.dir.vec[[3]]
-data.m.list <- FormatData(testFold.dir.vec[[3]])
-X.train <- data.m.list$X
-Y.train <- data.m.list$y
-
-set.seed(1)
-### FAILED
-cv.iregnet(X.train , Y.train , family= "gaussian" , scale_init= NA , estimate_scale = TRUE)
-cv.iregnet(X.train , Y.train , family= "gaussian" , scale_init= 1 , estimate_scale = FALSE)
-cv.iregnet(X.train , Y.train , family= "logistic" , scale_init= NA , estimate_scale = TRUE)
-cv.iregnet(X.train , Y.train , family= "logistic" , scale_init= 1 , estimate_scale = FALSE)
-
-### PASSED
-
+### split data into train and valid and test on different folds
 
 
