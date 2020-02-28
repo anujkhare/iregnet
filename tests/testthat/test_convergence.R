ad7199262a069085f2e841ddccbc102ffc30e0f9
Andrew Hurst <andrew_hurst92@yahoo.com>
2020-02-28 10:58:48 -0700

    resolving conflits 1.0

--- tests/testthat/test_convergence.R
+++ tests/testthat/test_convergence.R
@@ -2,10 +2,9 @@ context("iregnet prediction")
 library(iregnet)
 
 
-
 test_that("iregnet function converged", {
   
-  load("data/H3K36me3_TDH_immune.Rdata")
+  data( H3K36me3_TDH_immune , package = "iregnet")
   fit <- with(H3K36me3_TDH_immune , iregnet::iregnet(inputs , outputs , debug = 1))
   expect_equal(fit$error_status, 0)
 })
