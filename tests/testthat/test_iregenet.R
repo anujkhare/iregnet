1b6f1a25aa53b854224eaa90176817ec1dc1f077
Andrew Hurst <andrew_hurst92@yahoo.com>
2020-02-28 05:21:21 -0700

    fix test case 2.0

--- tests/testthat/test_iregenet.R
+++ tests/testthat/test_iregenet.R
@@ -3,10 +3,10 @@ library(iregnet)
 
 
 
-test_that( "iregnet function converged", {
+test_that("iregnet function converged", {
   
   load("data/H3K36me3_TDH_immune.Rdata")
-  fit <- with(H3K36me3_TDH_immune , iregnet(inputs , outputs))
+  fit <- with(H3K36me3_TDH_immune , iregnet::iregnet(inputs , outputs , debug = 1))
   expect_equal(fit$error_status, 0)
 })
 
