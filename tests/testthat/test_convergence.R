439dc5a6acebfa0c8c9dc969e9fc741a13ffe59c
Andrew Hurst <andrew_hurst92@yahoo.com>
2020-04-03 02:58:11 -0700

    fixed cpp compile warnings: unhandled distributions

--- tests/testthat/test_convergence.R
+++ tests/testthat/test_convergence.R
@@ -3,8 +3,8 @@ library(iregnet)
 
 
 test_that("iregnet function converged", {
-  
-  data( H3K36me3_TDH_immune , package = "iregnet")
+
+  data( H3K36me3_TDH_immune , package= "iregnet")
   fit <- with(H3K36me3_TDH_immune , iregnet::iregnet(inputs , outputs , debug = 1))
   expect_equal(fit$error_status, 0)
 })
