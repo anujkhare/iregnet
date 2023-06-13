a58f0596b6f8687487ca39f0ffe6313bcb4ea16c
Georg Heinze <georg.heinze@meduniwien.ac.at>
2023-06-13 16:46:27 +0200

    functions for lasso, relaxed lasso and adaptive lasso (with any distribution), and crossvalidation, but still without documentation

--- R/lasso.R
+++ R/lasso.R
@@ -26,7 +26,7 @@ relax.lasso <- function(x, y, family, ...){
     scale=sapply(1:num_lambda, function(a) {
             relaxfit[[a]]$scale
         })
-    return(list(coef=coef, scale=scale, lambda=fit$lambda, x=x, y=y, type="relax.lasso"))
+    return(list(coef=coef, scale=scale, lambda=fit$lambda, x=x, y=y, type="relax.lasso", family=family))
     }
 
 
@@ -39,7 +39,7 @@ ada.lasso <- function(x, y, family, omega=1, ...){
     ada.beta<-fit_lasso$beta
     ada.beta <- rbind(ada.beta[1,], diag(weights) %*% fit_lasso$beta[-1,])
     rownames(ada.beta) <- c("(Intercept)",colnames(x))
-    return(list(coef=ada.beta, scale=fit_lasso$scale, lambda=fit_lasso$lambda, omega=omega, x=x, y=y, type="ada.lasso"))
+    return(list(coef=ada.beta, scale=fit_lasso$scale, lambda=fit_lasso$lambda, omega=omega, x=x, y=y, type="ada.lasso", family=family))
 }
 
 plotlasso <- function(fit, xvar="log"){
@@ -56,7 +56,7 @@ plotlasso <- function(fit, xvar="log"){
     for(j in 1:nrow(fit$coef[-1,])) lines(x=xv, y=fit$coef[j+1,], type="l", lty=1, col=j)
 }
 
-cv.lasso <- function(fit, nfolds=10, foldid=NULL,  ...){
+cv.lasso <- function(fit, nfolds=10, foldid=NULL,  rep=1, ...){
     x <- fit$x
     y <- fit$y
     n <- nrow(x)
@@ -64,17 +64,69 @@ cv.lasso <- function(fit, nfolds=10, foldid=NULL,  ...){
     if(is.null(foldid)){
         foldid<-sample(rep(1:nfolds, each=ceiling(n/nfolds)), size=n, repl=FALSE) # may happen that not equally distributed
     } else nfolds <- length(unique(foldid))
-    loglik <- matrix(0, nrow=nfolds, ncol=num_lambda)
-    for(ifold in 1:nfolds){
-        is.train <- foldid!=ifold
-        is.valid <- foldid==ifold
-        if(fit$type=="lasso") fit.train <- lasso(x=x[is.train, ], y=y[is.train,], family=fit$family, ...)
-        if(fit$type=="relax.lasso") fit.train <- relax.lasso(x=x[is.train, ], y=y[is.train,], family=fit$family, ...)
-        if(fit$type=="ada.lasso") fit.train <- ada.lasso(x=x[is.train, ], y=y[is.train,], omega=omega, family=fit$family, ...)
-        loglik[ifold,] <- LAPPLY(1:num_lambda, function(a) survreg(y[is.valid,]~x[is.valid], dist=family, init=fit.train$coef[,a], scale=fit.train$scale[a], estimate.scale=FALSE, control=survreg.control(maxiter=0)))
+    loglik <- matrix(0, nrow=nfolds*rep, ncol=num_lambda)
+    for(irep in 1:rep){
+        for(ifold in 1:nfolds){
+            is.train <- foldid!=ifold
+            is.valid <- foldid==ifold
+            if(fit$type=="lasso") fit.train <- lasso(x=x[is.train, ], y=y[is.train,], ...)
+            if(fit$type=="relax.lasso") fit.train <- relax.lasso(x=x[is.train, ], y=y[is.train,], ...)
+            if(fit$type=="ada.lasso") fit.train <- ada.lasso(x=x[is.train, ], y=y[is.train,], omega=omega,  ...)
+            loglik[ifold,] <- unlist(LAPPLY(1:num_lambda, function(a) 
+                survreg(y[is.valid,]~x[is.valid,], dist=fit$family, init=fit.train$coef[,a], 
+                        scale=fit.train$scale[a], control=survreg.control(maxiter=0))$loglik[2]))
+        }
     }
-    cvm <- apply(loglik,2,sum)
+    cvm <- apply(loglik,2,mean)*nfolds
     cvm.sd <- apply(loglik,2,sd)
-    s <- fit$lambda[which.max(cvm)]
-    return(list(lasso.fit = fit, cvm=cvm, cvm.sd=svm.sd, s=s, lambda.min=fit$lambda[s], type=fit$type, family=fit$family))
+    s <- which.max(cvm)
+    return(list(lasso.fit = fit, cvm=cvm, cvm.sd=cvm.sd, s=s, lambda.min=fit$lambda[s], type=fit$type, family=fit$family))
 }
+
+plot.cv.lasso <- function(fit, adj=5){
+    num_lambda <- length(fit$lasso.fit$lambda)
+    active <- apply(fit$lasso.fit$coef[-1,], 2, function(X) sum(X!=0))
+    xv <- log(fit$lasso.fit$lambda+fit$lasso.fit$lambda[num_lambda-1]*adj)
+    ats <- c(xv[1], seq(xv[2], xv[num_lambda], length.out=5)) 
+    iats <- c(1, seq(2, num_lambda, length.out=10))
+    labs <- round(exp(ats)-fit$lasso.fit$lambda[num_lambda-1]*adj,4)
+    ylim <- c(min(fit$cvm - fit$cvm.sd), max(fit$cvm + fit$cvm.sd))
+    plot(xv, fit$cvm, type="o", ylim=ylim, axes=F, ylab="CV log Likelihood", xlab="lambda", col="red")
+    axis(2)
+    for(i in 1:length(fit$lasso.fit$lambda)) lines(c(xv[i], xv[i]), c(fit$cvm[i]-fit$cvm.sd[i], fit$cvm[i]+fit$cvm.sd[i]), col="black") 
+    axis(1, at=ats, labels=labs)
+    abline(h=fit$cvm[fit$s], lty=3)
+    abline(v=xv[fit$s], lty=3)
+    points(xv[fit$s], fit$cvm[fit$s], pch=18, col="red")
+    axis(3, at=xv[iats], labels=active[iats])
+    box()
+}
+
+coef.cv.lasso <- function(fit){
+    return(fit$lasso.fit$coef[,fit$s])
+}
+
+scale.cv.lasso <- function(fit){
+    return(fit$lasso.fit$scale[fit$s])
+}
+
+predict.cv.lasso <- function(fit, newdata, type="response", ...){
+    # uses predict.survreg
+    if(missing(newdata)) newdata <- data.frame(fit$lasso.fit$x)
+    active <- (coef.cv.lasso(fit)[-1]!=0)
+    y <- fit$lasso.fit$y
+    x <- data.frame(fit$lasso.fit$x)
+    if(sum(active)==0) fit0 <- survreg(fit$lasso.fit$y~1, dist=fit$family, scale=fit$lasso.fit$scale)
+    else {
+        fit0 <- survreg(as.formula(paste("y~",paste(names(active)[active], collapse="+"))), 
+                        data=x,dist=family, init=coef.cv.lasso(fit)[c(TRUE,active)], scale=scale.cv.lasso(fit), control=survreg.control(maxiter=0))
+    }
+    p<-predict(fit0, newdata, type, ...)
+    return(p)
+}
+
+surv.cv.lasso <- function(fit, newdata, time, grid=seq(0.001, 0.999, 0.001), se=TRUE){
+    quant<-predict.cv.lasso(fit, newdata=newdata, type="quantile", p=grid, se=TRUE)
+    return(unlist(sapply(1:nrow(quant$fit), function(X) 1-max(tail(grid[quant$fit[X,]<=time],1),0))))
+}
+
